1561f6fbe62910d5a3ebdf9a2b88ba8af84eba44
martinlugton <martinlugton@gmail.com>
2022-10-02 08:59:17 +0000

    Iterate through the API response object and console.log the data we are interested in

--- index.js
+++ index.js
@@ -7,11 +7,18 @@ const axios = require('axios');
 var makeGetRequest = async function (request) {
     try {
         response = await axios.get(request);
-        console.log(response.data);
+	//console.log(response.data);
+	for (const result_item of response.data) {
+		console.log("Destination: " + result_item.destinationName);
+	        console.log("Platform: " + result_item.platformName);
+        	console.log("Expected Arrival: " + result_item.expectedArrival);
+        	console.log("Time to station: " + result_item.timeToStation);
+		console.log("\n");
+	}
     } catch (err) {
        console.error(err);
     }
 
 }
 
-makeGetRequest("https://api.tfl.gov.uk/Line/london-overground/Arrivals/HUBNWD?direction=inbound" + "&api_key=" + api_key) 
+makeGetRequest("https://api.tfl.gov.uk/Line/london-overground/Arrivals/HUBNWD?direction=inbound" + "&api_key=" + api_key); 
